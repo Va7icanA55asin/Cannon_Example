db6fbb6438bb2a7407cbd666b58db41b3b923187
Gabriel Blandin <gblandin@iastate.edu>
2022-01-22 14:36:12 -0600

    Initial Commit containing both examples I worked on

---
+++ trick_sims/SIM_cannon_analytic/RUN_test/input.py
@@ -0,0 +1,5 @@
+#exec(open("Modified_data/cannon.dr").read())
+execfile("Modified_data/realtime.py")
+execfile("Modified_data/cannon.dr")
+trick.trick_view_add_auto_load_file("TV_cannon.tv")
+trick.stop(5.2)
